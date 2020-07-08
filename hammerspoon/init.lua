02afbc19b696d8c1fe123c95f5620fea418af4b0
Kyriacos Souroullas <kyriacos.souroullas@gmail.com>
2020-07-08 07:38:19 -0700

    Update hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -21,6 +21,22 @@ end)
 -- this is here since any code after hs.reload() would not be called. hs.reload() destroys the current Lua interpreter
 hs.alert.show("Config loaded")
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
+  hs.reload()
+end)
+
+local display = 17;
+hs.hotkey.bind({"ctrl", "cmd", "shift"}, "`", function()
+-- hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Help", function()
+  if display == 17 then
+    display = 15
+  else
+    display = 17
+  end
+  hs.execute("/Users/kaks/code/ddcctl/ddcctl -d 1 -i " .. display)
+  -- hs.eventtap.keyStroke({}, "ctrl", "cmd"
+  -- https://gist.github.com/nonissue/5e6d2de97d24e31347483365911a919c
+end)
 -- open -g hammerspoon://someAlert
 -- hs.urlevent.bind("someAlert", function(eventName, params)
 --     hs.alert.show("Received someAlert")
