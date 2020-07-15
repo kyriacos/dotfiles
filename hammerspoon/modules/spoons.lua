49c3fd0f6502e95fdd8858d92095b5c0224fdccf
Kyriacos Souroullas <213004+kyriacos@users.noreply.github.com>
2020-07-15 08:04:11 -0700

    Disable hammerspoon rounded corners

--- hammerspoon/modules/spoons.lua
+++ hammerspoon/modules/spoons.lua
@@ -1,7 +1,7 @@
 -- -- load spoons
 hs.loadSpoon("Calendar")
 -- hs.loadSpoon("CircleClock")
-hs.loadSpoon("RoundedCorners")
+-- hs.loadSpoon("RoundedCorners")
 -- hs.loadSpoon("WindowHalfsAndThirds")
 -- hs.loadSpoon("TimeFlow")
 
@@ -10,4 +10,4 @@ hs.loadSpoon("ModalMgr")
 hs.loadSpoon("HSearch")
 
 
-spoon.RoundedCorners:start()
+-- spoon.RoundedCorners:start()
