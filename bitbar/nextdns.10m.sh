4188488df439b4dd1c97c98d0d86bc0bfc64fef1
Kyriacos Souroullas <kyriacos.souroullas@gmail.com>
2020-07-15 08:11:34 -0700

    Add bitbar plugin for nextdns

---
+++ bitbar/nextdns.10m.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "NDNS"
+echo "---"
+curl -m 1 https://link-ip.nextdns.io/d482b9/88e510eafecc7334 2>/dev/null && echo "$2"
+echo "---"
+echo "Update | href=https://link-ip.nextdns.io/d482b9/88e510eafecc7334"
+echo "---"
